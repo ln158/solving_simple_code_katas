82ec0fc0efe55f42f86406d7477bb3c2821ac491
lina zhao <lnzhao2020@gmail.com>
2026-01-28 13:06:15 +0000

    Finish exercise2

---
+++ exercise2.py
@@ -0,0 +1,20 @@
+# Build a program that calculates the number of characters in a string
+# eg. count_characters("its a fantastic day", "a") #=> 4
+
+# Parameters
+#    - text, a string (eg. "life is amazing")
+#    - character, a string (eg. "e")
+# Returns
+#    - integer (eg. 4)
+# Side effects
+#    - None
+def count_characters(text, character):
+    count = 0
+    for letter in text: 
+        if letter==character:
+            count +=1
+    return count
+print(count_characters("Its a fantastic day", "a"))
+
+# Tasks 
+# - 
